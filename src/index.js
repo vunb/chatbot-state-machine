026d8633bb257b717291c1bb35613645543c042a
weihanchen <willhanchen@gmail.com>
2018-06-10 08:09:12 +0800

    fix port format

--- src/index.js
+++ src/index.js
@@ -76,9 +76,9 @@ const newFaq = () => new StateMachine('start', {
         onApplyFormalcard: () => ({
             text: `首次申請信用卡正卡須年滿20歲（鼎極卡須年滿24歲），請備妥申請人身分證正反面影本、財力證明文件，即可申辦。`
         }),
-        onApplyBackcard: () => ([
+        onApplyBackcard: () => ({
             text: `申請他人信用卡附卡，須年滿 15 歲，且為正卡之配偶、父母、子女、兄弟姊妹及配偶之父母，請備妥附卡申請人身分證正反面影本、可佐證正附卡關係之證明文件，如正卡人之身份證影本及戶籍謄本(毋需附財力資料)。`
-        ])
+        })
     }
 });
 bot.onEvent(async context => {
@@ -99,59 +99,6 @@ bot.onEvent(async context => {
 
     if (!labelSlot.hasOwnProperty(label)) context.response.body = '';
     else context.response.body = await labelSlot[label]();
-
-    // console.log(fastTextPath)
-    // const faseText = spawn(fastTextPath, [
-    //     'predict-prob',
-    //     'faq2.model.bin',
-    //     '-',
-    //     '5'
-    // ]);
-
-    // faseText.stdin.setEncoding('utf-8');
-    // faseText.stdout.pipe(process.stdout);
-
-    // faseText.stdin.write(context.event.text);
-
-    // faseText.stdin.end();
-    // faseText.on('data', data => {
-    //     console.log(data)
-    // })
-
-    // faseText.stdin.write(context.event.text);
-    // faseText.on('data', (data) => {
-    //     console.log(data.toString())
-    // })
-    // faseText.on('error', error => {
-    //     console.log(error)
-    // })
-    // const { text: label } = context.event;
-    // const labelSlot = {
-    //     __label__payment: () =>
-    //         context.setState({ payment: true, email: false, address: false }),
-    //     __label__email: () =>
-    //         context.setState({ email: true, payment: false, address: false }),
-    //     __label__address: () =>
-    //         context.setState({ address: true, payment: false, email: false }),
-    //     default: () => context.resetState()
-    // };
-
-    // if (labelSlot.hasOwnProperty(label)) labelSlot[label]();
-    // else labelSlot['default']();
-    // const { payment, email, address } = context.state;
-    // if (payment) {
-    //     context.response.body = {
-    //         text: '申請帳單可以使用以下方式:',
-    //         options: ['電子帳單', '實體帳單']
-    //     };
-    //     if (email) {
-    //         context.response.body = `(1)您如果是本行網路銀行客戶，可選擇直接上網辦理申請
-    //         (2)您如果尚未成為本行網路銀行客戶，則可選擇先備妥您的信用卡卡號，註冊為網路銀行客戶後，直接上網辦理申請電子信用卡帳單。 (註冊網銀客戶與電子帳單服務僅限信用卡正卡持卡人)。
-    //         (3)如果客戶不欲註冊網銀，亦可由客服人員幫其設定。`;
-    //     } else if (address) {
-    //         context.response.body = `您可透過語音或透過客服人員補寄本期或申請1年半內的帳單(補寄3個月以前帳單，每個月份發補手續費100元)`;
-    //     }
-    // }
 });
 const server = createServer(bot);
 
